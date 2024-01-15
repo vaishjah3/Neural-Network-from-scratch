bea96b15f3b7f530968bd0cfe1b7ab7b320f5058
Vaishnavi Jahagirdar <vaish.jah@gmail.com>
2024-01-15 09:42:03 -0800

    Create neuron.py

---
+++ neuron.py
@@ -0,0 +1,7 @@
+#This defines a basic structure of a neuron somewhere in between a Network Network
+inputs=[1.2, 1.5, 1.9]
+weight=[1.2,3.4, 5,6]
+bias=2
+
+output= inputs[0]*weight[0]+inputs[1]*weight[1]+inputs[2]*weight[2]+bias
+print(output)
